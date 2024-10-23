6d796b2a1281f838269abbb707e3c51f97c8f02c
Tuan Trinh <TuanTrinh@Polymath>
2024-10-23 13:12:36 +0700

    add

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TNYouTubeKit",
-            targets: ["YouTubeKit"]),
+            targets: ["TNYouTubeKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,7 +25,7 @@ let package = Package(
             name: "TNYouTubeKit",
             dependencies: []),
         .testTarget(
-            name: "YouTubeKitTests",
+            name: "TNYouTubeKitTests",
             dependencies: ["TNYouTubeKit"]),
     ]
 )
