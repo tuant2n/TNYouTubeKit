9cd830e70211f10f3d6d73ab98cadd6a3c456810
Tuan Trinh <TuanTrinh@Polymath>
2024-10-23 13:06:04 +0700

    fix

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "YouTubeKit",
+    name: "TNYouTubeKit",
     /*platforms: [
         .macOS(.v12), .iOS(.v15), .watchOS(.v8), .tvOS(.v15)
     ],*/
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "YouTubeKit",
+            name: "TNYouTubeKit",
             targets: ["YouTubeKit"]),
     ],
     dependencies: [
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "YouTubeKit",
+            name: "TNYouTubeKit",
             dependencies: []),
         .testTarget(
             name: "YouTubeKitTests",
